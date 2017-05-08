b8957957c8f7311d8fe22333394a5bb8557f0ebe
mariana-martins <mariana_martins@id.uff.br>
2017-05-08 14:06:52 +1200

    Get elements

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+var player = document.querySelector(".player");
+var video = player.querySelector(".viewer");
+var progress = player.querySelector(".progress");
+var progressBar = player.querySelector(".progress__filled");
+var toggle = player.querySelector(".toggle");
+var skipButtons = player.querySelectorAll("[data-skip]");
+var ranges = player.querySelectorAll(".player__slider");
