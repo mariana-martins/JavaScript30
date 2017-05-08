de60ba23b3e7973e87d5f5d1f5c02c5914339166
mariana-martins <mariana_martins@id.uff.br>
2017-05-08 14:11:00 +1200

    Add comments and add togglePlay function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,3 +1,4 @@
+// Get our elements
 var player = document.querySelector(".player");
 var video = player.querySelector(".viewer");
 var progress = player.querySelector(".progress");
@@ -5,3 +6,13 @@ var progressBar = player.querySelector(".progress__filled");
 var toggle = player.querySelector(".toggle");
 var skipButtons = player.querySelectorAll("[data-skip]");
 var ranges = player.querySelectorAll(".player__slider");
+
+//Build our functions
+function togglePlay() {
+    if (video.paused){
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+// Hook up the event listners
