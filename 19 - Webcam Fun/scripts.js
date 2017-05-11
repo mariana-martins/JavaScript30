1f96d3726e84a690051130336f7457d16e499ea6
mariana-martins <mariana_martins@id.uff.br>
2017-05-12 11:34:18 +1200

    Create a getVideo function and descomment divs in index file. Now is possible to see the image from webcam on the screen.

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,19 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+var video = document.querySelector('.player');
+var canvas = document.querySelector('.photo');
+var ctx = canvas.getContext('2d');
+var strip = document.querySelector('.strip');
+var snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(function (localMediaStream) {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(function (error) {
+            console.error("This is a error message!", error);
+        });
+}
+
+getVideo();
